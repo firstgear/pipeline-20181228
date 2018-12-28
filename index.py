62a2de75843e2d0da6cec4b6efa55e145daab66f
firstgear98 <firstgear@users.noreply.github.com>
2018-12-28 15:21:43 +0100

    using json.dumps to dump body

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
+import json
 def handler(event, context):
     message = "Hello, is it me your're looking for?"
     return {
         "statusCode": 200,
-        "body" : message
+        "body" : json.dumps(message)
     }
-
