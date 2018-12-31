3218d49cbdcbf37d3a822392714da7ec18412cab
firstgear98 <firstgear@users.noreply.github.com>
2018-12-31 12:55:56 +0100

    add date to Hello

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Hello, is it me your're looking for?"
+    message = "Hello, is it me your're looking for? Dec 31."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
