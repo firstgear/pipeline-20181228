c7ea1ce852e4d39d8ad2dd4b1f1668bb98818696
firstgear98 <firstgear@users.noreply.github.com>
2019-01-10 16:28:23 +0100

    in 2018 commit

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Hello, is it me your're looking for? Dec 31."
+    message = "Hello, is it me your're looking for? 9 Jan."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
