3bf9a8c5be0ef3026ac85bb9e093fef30f2eeb6f
firstgear98 <firstgear@users.noreply.github.com>
2019-02-13 22:39:31 +0100

    Welcome to RC1

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Welcome to release candidate."
+    message = "Welcome to release candidate2."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
