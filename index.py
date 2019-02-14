9160636ed3b542b6574d3e3166448ef005d69c72
firstgear98 <firstgear@users.noreply.github.com>
2019-02-14 18:27:13 +0100

    Welcome bij ons

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Welcome to release candidate2."
+    message = "Welcome to release candidate3."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
