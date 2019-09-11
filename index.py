db5baef28a2c493170a7f2c63b84b206e3eaa1e0
firstgear98 <firstgear@users.noreply.github.com>
2019-09-11 22:49:57 +0200

    Welcome to your API

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Welcome to release candidate3."
+    message = "Welcome to SAM driven API."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
