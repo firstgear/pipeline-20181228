90494184fa5e16b58270be83349b96e5474805c2
firstgear98 <firstgear@users.noreply.github.com>
2019-09-12 11:20:04 +0200

    type change of topic

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Welcome to SAM driven APY."
+    message = "Welcome to Knowledge Bite session on public cloud."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
