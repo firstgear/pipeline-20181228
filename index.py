781989283290d725e57ab8698457b8d38ed95348
jmmeulem <jmmeulem@amazon.com>
2019-11-06 20:24:17 +0100

    round three :)

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 def handler(event, context):
-    message = "Welcome to Knowledge Bite session on public cloud2."
+    message = "Welcome to Knowledge Bite session on public cloud3."
     return {
         "statusCode": 200,
         "body" : json.dumps(message)
